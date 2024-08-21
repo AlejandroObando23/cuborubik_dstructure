7c4e10b81bbf22400fbc0201aeef8abc51c7ac48
FloresCaetano <caetano.flores@hotmail.com>
2024-08-21 11:30:24 -0500

    implementacion_librerias_openGL, Glad, glfw, glm

--- code/Rubik/JuegoRubik/main.cpp
+++ code/Rubik/JuegoRubik/main.cpp
@@ -1,3 +1,16 @@
+// OpenGL
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+#include <glm/glm/glm.hpp>
+#include <glm/glm/gtc/matrix_transform.hpp>
+#include <glm/glm/gtc/type_ptr.hpp>
+
+// STD libraries
+#include <iostream>
+#include <random>
+#include <vector>
+#include "defineRubik.h"
+
 int main()
 {
     return 0;
