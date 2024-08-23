2591ffc721cbddeba729ef7908394ec7a4592555
AlejandroObando23 <alejoobando21@gmail.com>
2024-08-22 19:23:34 -0500

    implementacion_primera_ventana_y_rendirazado

--- code/Rubik/JuegoRubik/main.cpp
+++ code/Rubik/JuegoRubik/main.cpp
@@ -5,13 +5,117 @@
 #include <glm/glm/gtc/matrix_transform.hpp>
 #include <glm/glm/gtc/type_ptr.hpp>
 
+// Rubik cube
+#include "RubikCube.h"
+
 // STD libraries
 #include <iostream>
 #include <random>
 #include <vector>
 #include "defineRubik.h"
 
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+bool pressedSolve = false;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
 int main()
 {
+    // inicializar glfw
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // crear ventana con glfw 
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Rubik Cube", NULL, NULL);
+    if (window == NULL) 
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // CONFIGURANDO GLFW PARA CAPTURAR EL MOUSE
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // glad: cargar todos los punteros de GLAD
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // configurar el estado global de openGL
+
+
+
+    // build and compile our shader zprogram
+    // ------------------------------------
+    GLuint VBO[1], VAO[1], EBO[1];
+    glGenVertexArrays(1, VAO);
+    // atributo de color
+    //asociamos el VAO correspondiente
+    
+    glLineWidth(10.0f); // Delineado del cubo
+    // loop de renderizado
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // Obtener delta time
+        // --------------------
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // Procesar las entradas (teclado, raton etc)
+        // ----
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // camera/view transformaciones
+
+
+        glfwSwapBuffers(window); //Refrescar la pantalla
+        glfwPollEvents();
+    }
+
+    // Limpiar recuersos utilizados al cerrar el programa
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, VAO);
+    glDeleteBuffers(1, VBO);
+    glDeleteBuffers(1, EBO);
+    glfwTerminate();
     return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// --------------------------------------------------------------------------------------------------------
+
+// glfw: Funcion que se llama cada vez que actualizamos el tamaño de la ventana
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    //actualizar el tamaño del viewport de openGl segun las nuevas dimensiones
+    glViewport(0, 0, width, height);
 }
