4f0fe75d4f507676cef679fee93f37f58d7f54b6
AlejandroObando23 <alejoobando21@gmail.com>
2024-08-30 23:23:43 -0500

    implementacion_controles

--- code/Rubik/JuegoRubik/camera.h
+++ code/Rubik/JuegoRubik/camera.h
@@ -50,6 +50,18 @@ public:
     // returns the view matrix calculated using Euler Angles and the LookAt Matrix
     glm::mat4 GetViewMatrix();
 
+    // processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
+    void ProcessKeyboard(Camera_Movement direction, float deltaTime);
+
+    // processes input received from a mouse input system. Expects the offset value in both the x and y direction.
+    void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true);
+
+    // processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
+    void ProcessMouseScroll(float yoffset);
+
+private:
+    // calculates the front vector from the Camera's (updated) Euler Angles
+    void updateCameraVectors();
 };
 
 #endif // !CAMERA_H_
